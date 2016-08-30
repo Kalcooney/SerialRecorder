c63ec62541622e815705e479356812d947c118cb
Kaleb Cooper <kalebcooper@gmail.com>
2016-08-30 19:30:49 +1200

    Initial commit

---
+++ app/src/main/java/com/example/kaleb/serialrecorder/MainMenu.java
@@ -0,0 +1,34 @@
+package com.example.kaleb.serialrecorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    //onClick method for the New Item Button. When clicked goes to the New Item Activity
+    public void newItemClicked(View view){
+
+    }
+
+    //onClick method for the View Items Button. When clicked goes to the View Items Activity
+    public void viewItemsClicked(View view){
+
+    }
+
+    //onClick method for the Help Button. When clicked goes to the Help Activity
+    public void helpClicked(View view){
+
+    }
+
+    //onClick method for the About Activity. When clicked goes to the About Activity
+    public void aboutClicked(View view){
+
+    }
+}
