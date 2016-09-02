f593ee3d3c95637456cef71570b720a9a44216d0
Kaleb Cooper <kalebcooper@gmail.com>
2016-09-03 00:09:45 +1200

    Allow user to take photo and place it in an Imageview

--- app/src/main/java/com/example/kaleb/serialrecorder/NewItemActivity.java
+++ app/src/main/java/com/example/kaleb/serialrecorder/NewItemActivity.java
@@ -1,19 +1,29 @@
 package com.example.kaleb.serialrecorder;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class NewItemActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     //declare the views we are going to use
     EditText itemNameInput;
     EditText itemDescriptionInput;
     EditText serialNumberInput;
+    ImageView itemImageView;
     MyDBHandler dbHandler;
 
     @Override
@@ -22,10 +32,40 @@ public class NewItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_item);
 
         //reference the views we are going to use
+        Button takePhotoButton = (Button) findViewById(R.id.takePhotoButton);
         itemNameInput = (EditText) findViewById(R.id.itemNameInput);
         itemDescriptionInput = (EditText) findViewById(R.id.itemDescriptionInput);
         serialNumberInput = (EditText) findViewById(R.id.serialNumberInput);
+        itemImageView = (ImageView) findViewById(R.id.itemImageView);
         dbHandler = new MyDBHandler(this, null, null, 1);
+
+        //disable the buttons if the user doesn't have a camera
+        if(!hasCamera())
+            takePhotoButton.setEnabled(false);
+
+        }
+
+    //check if user has a camera
+    private boolean hasCamera(){
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
+
+    public void launchCamera(View view){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //take picture and pass it on to onActivityResult
+        startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    //return image taken
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            //get the photo
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap) extras.get("data");
+            itemImageView.setImageBitmap(photo);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     //add actions to the action bar
@@ -47,6 +87,7 @@ public class NewItemActivity extends AppCompatActivity {
                 startActivity(mainMenu);
                 Toast.makeText(this, "Successfully added", Toast.LENGTH_LONG).show();
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
