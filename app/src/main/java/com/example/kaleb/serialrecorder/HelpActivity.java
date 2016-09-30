969d8ccea2316386ea2a5e7427fe81e62ec25f45
Kaleb Cooper <kalebcooper@gmail.com>
2016-10-01 02:18:46 +1300

    Add functionality to Help Activity

--- app/src/main/java/com/example/kaleb/serialrecorder/HelpActivity.java
+++ app/src/main/java/com/example/kaleb/serialrecorder/HelpActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
@@ -14,21 +16,10 @@ public class HelpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_help);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //reference the Textview containing the helpText
-        TextView helpText = (TextView) findViewById(R.id.helpText);
-        //set a whole bunch of sample text
-        helpText.setText("This is some sample text. This section will eventually contain " +
-                "information on how to use the app. Lorem ipsum dolor sit amet, consectetur " +
-                "adipiscing elit. Mauris laoreet scelerisque vulputate. Nunc ut magna a nulla " +
-                "imperdiet vehicula et vel magna. Sed ac ligula turpis. Nulla a sapien venenatis " +
-                "dui pellentesque facilisis hendrerit vitae tortor. Phasellus iaculis velit sed " +
-                "erat iaculis, nec porttitor magna hendrerit. Morbi porttitor velit nisi, non " +
-                "ullamcorper lectus commodo et. Vestibulum convallis urna sed augue iaculis " +
-                "maximus. Donec a turpis vulputate urna faucibus vehicula. Quisque fermentum " +
-                "suscipit augue lobortis congue. Fusce interdum, orci et elementum maximus, orci " +
-                "ipsum lacinia nulla, in fringilla mi nibh id neque. Aenean quam nulla, cursus " +
-                "non nibh a, porta lobortis elit. Nulla condimentum luctus libero. Sed aliquam " +
-                "purus orci.");
+        WebView browser = (WebView) findViewById(R.id.webView);
+        WebSettings settings = browser.getSettings();
+        settings.setJavaScriptEnabled(true);
+        browser.loadUrl("file:///android_asset/help.html");
     }
 
     //to handle up arrow event
