f07469691d813bd4796246c230e27af7c0590ded
Kaleb Cooper <kalebcooper@gmail.com>
2016-09-30 18:58:01 +1300

    Add Overflow menu to SingleItemActivity

--- app/src/main/java/com/example/kaleb/serialrecorder/SingleItemActivity.java
+++ app/src/main/java/com/example/kaleb/serialrecorder/SingleItemActivity.java
@@ -1,8 +1,11 @@
 package com.example.kaleb.serialrecorder;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,13 +14,14 @@ public class SingleItemActivity extends AppCompatActivity {
 
     ImageView itemImage;
     MyDBHandler dbHandler;
+    private String getId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_item);
 
-        String getId = getIntent().getExtras().getString("ID");
+        getId = getIntent().getExtras().getString("ID");
         int id = Integer.parseInt(getId);
 
         //reference the text and image views
@@ -44,4 +48,21 @@ public class SingleItemActivity extends AppCompatActivity {
         itemImage.setImageURI(imageUri);
     }
 
+    //add actions to the action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.single_item_activity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //if action is selected, perform an action
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case(R.id.edit_item):
+                Intent editItemActivity = new Intent(this, EditItemActivity.class);
+                startActivity(editItemActivity);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
